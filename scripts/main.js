ef14d5fb76a4b0a1cfca7aefd68c439476113a37
Ray Orunsolu <rorunsolu@gmail.com>
2024-10-02 17:43:46 +0100

    removed scss variables & restructured footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
+
+import '/styles/utilities.scss';
 import '/styles/style.scss';
-import '/styles/layout.scss';
-import '/styles/variables.scss';
 
 import '/styles/navbar.scss';
 import '/styles/header.scss';
+import '/styles/footer.scss';
+
 import '/styles/services.scss';
 import '/styles/projects.scss';
 import '/styles/blogs.scss';
 import '/styles/contact.scss';
-import '/styles/footer.scss';
+
